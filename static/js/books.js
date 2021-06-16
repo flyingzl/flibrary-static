c43c3f6c3b67adae627443b60d543620647f1ca9
zhaolei <zhaolei@migu.cn>
2021-06-16 20:28:54 +0800

    feat: list books by category

--- static/js/books.js
+++ static/js/books.js
@@ -7,6 +7,9 @@
         setup() {
             const originBooks = Vue.ref(window.books)
             const filterName = Vue.ref('')
+            const categories = Vue.ref(window.categories)
+
+
             const books = Vue.computed(function () {
                 return originBooks.value.filter(function (book) {
                     if (!filterName.value) return true
@@ -14,10 +17,35 @@
                 })
             })
 
+            const currentCategoryName = Vue.computed(() => {
+                const id = new URLSearchParams(location.search).get('category')
+                if (!id) return  ''
+                const category = categories.value.find(category => category.id === id)
+                return category && category.name
+            })
+
+            const AllBtnClass = Vue.computed(() => {
+                return {
+                    'btn-main-sm': !currentCategoryName.value,
+                    'btn-normal-sm': currentCategoryName.value,
+                }
+            })
+
+            const CategoryBtnClass = Vue.computed(() => {
+                return {
+                    'btn-main-sm': currentCategoryName.value,
+                    'btn-normal-sm': !currentCategoryName.value,
+                }
+            })
+
+
             return {
-                originBooks,
                 books,
                 filterName,
+                categories,
+                currentCategoryName,
+                AllBtnClass,
+                CategoryBtnClass
             }
         },
     })
